52daeff47608e9c823a56d6179327bd97f3a6827
ArseneSoulie <arsene.soulie@blablacar.com>
2025-07-23 15:45:24 +0200

    Initial Commit

---
+++ serena/serenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  serenaApp.swift
+//  serena
+//
+//  Created by A S on 23/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct serenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
