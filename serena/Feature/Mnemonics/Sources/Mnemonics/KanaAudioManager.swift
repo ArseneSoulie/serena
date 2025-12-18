06cf77928e9ea3917dc549c9cfd559d3be2cf087
ArseneSoulie <arsene.soulie@blablacar.com>
2025-12-18 17:07:59 +0100

    [fix] bundle reference

--- serena/Feature/Mnemonics/Sources/Mnemonics/KanaAudioManager.swift
+++ serena/Feature/Mnemonics/Sources/Mnemonics/KanaAudioManager.swift
@@ -5,7 +5,7 @@ class KanaAudioManager {
     var audioPlayer: AVAudioPlayer?
 
     func play(kanaString: String) {
-        guard let audioUrl = Bundle.module.url(forResource: kanaString, withExtension: "mp3") else { return }
+        guard let audioUrl = mnemonicsModule.url(forResource: kanaString, withExtension: "mp3") else { return }
         audioPlayer = try? AVAudioPlayer(contentsOf: audioUrl)
         audioPlayer?.play()
     }
